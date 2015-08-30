2af78e46ac842ea91dd727e47970303e0ceb5fbb
imruahmed <im.ahmed97@gmail.com>
2015-08-30 12:30:23 -0400

    Gallery working

--- app/src/main/java/io/github/imruahmed/noswipe/MainActivity.java
+++ app/src/main/java/io/github/imruahmed/noswipe/MainActivity.java
@@ -3,8 +3,13 @@ package io.github.imruahmed.noswipe;
 
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.support.v4.app.FragmentActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +21,20 @@ public class MainActivity extends AppCompatActivity {
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
         fragmentTransaction.replace(android.R.id.content, new PhotoGallery()).commit();
+
+    }
+
+    public void selectImage(View view) {
+
+        Animation animation = AnimationUtils.loadAnimation(getBaseContext(), android.R.anim.fade_out);
+        view.startAnimation(animation);
+
+    }
+
+    public void startGallery(View view) {
+
+        Intent i = new Intent(this, GalleryPager.class);
+        startActivity(i);
 
     }
 }
--- app/src/main/java/io/github/imruahmed/noswipe/PhotoGallery.java
+++ app/src/main/java/io/github/imruahmed/noswipe/PhotoGallery.java
@@ -11,6 +11,8 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ListAdapter;
